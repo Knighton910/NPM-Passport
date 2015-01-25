3e5522c9824092645b1b18520fd3d34a0fda13db
kelthenoble <keltheceo@gmail.com>
2015-01-25 00:36:12 -0800

    updating the app

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 var express = require('express');
 
+var passport = require('passport');
+var passportlocal = require('passport-local');
+
+
 var app = express();
 
 app.set('view engine', 'ejs')
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', function(req, res){
-	res.render('index');
+	res.render('index', {
+		isAuthenticated: false,
+		user: req.user
+	});
+});
+
+app.get('/login', function(req, res) {
+	res.render('login');
+});
+
+app.post(/login', function(req, res) {
+	 
 });
 
 var port = process.env.PORT || 1337;
